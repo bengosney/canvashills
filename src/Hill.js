e4405c17f85558a769ce35021fb60f37a00f2fd5
Ben Gosney <bengosney@googlemail.com>
2019-04-26 08:45:41 +0100

    Add trees

--- src/Hill.js
+++ src/Hill.js
@@ -1,5 +1,6 @@
 import noisyLine from './noisyLine';
 import Context from './Context';
+import Tree from './Tree';
 
 
 class Hill {
@@ -17,14 +18,22 @@ class Hill {
 	const line = new noisyLine(0, y, width, y, 50, 1);
 
 	ctx.beginPath();
-	line.draw();
+	ctx.fillStyle = colour;
+	line.draw((x, y) => {
+	    const randomRange = (min, max) => {
+		return min + Math.random() * (max - min);
+	    };
+	    
+	    if (randomRange(0, 100) > 95) {
+		const tree = new Tree(x, y + 3, randomRange(2, 5), null, Math.floor(randomRange(4, 6)));
+		tree.draw();
+	    }
+	});
 	ctx.lineTo(width, height);
 	ctx.lineTo(0, height);
 	ctx.lineTo(0, y);
-	ctx.fillStyle = colour;
 	ctx.fill();
 	ctx.closePath();
-    
     }
 }
 
