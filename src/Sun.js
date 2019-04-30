2367b0391782ce73fb61c2f1efbfe8241245a655
Ben Gosney <bengosney@googlemail.com>
2019-04-30 09:01:34 +0100

    Class for drawing the sun

---
+++ src/Sun.js
@@ -0,0 +1,30 @@
+import Context from './Context';
+
+class Sun {
+    constructor(x, y, size) {
+	this.x = x;
+	this.y = y;
+	this.size = size;
+    }
+
+    draw() {
+	const ctx = Context.get();
+	const { x, y, size } = this;
+
+	const rad = size / 2;
+
+	const grd = ctx.createRadialGradient(x, y, 0, x, y, size);
+      
+	grd.addColorStop(0, 'rgba(255, 255, 255, 1.000)');
+	grd.addColorStop(0.75, 'rgba(255, 250, 230, 1.000)');
+	grd.addColorStop(1, 'rgba(255, 255, 255, 0.000)');
+      
+	ctx.fillStyle = grd;
+
+	ctx.beginPath();
+	ctx.arc(x, y, size, 0, 2 * Math.PI);
+	ctx.fill();
+    }
+}
+
+export default Sun;
