ab772cd764becce70e0a434a111a7fe0a9c452a9
Ben Gosney <bengosney@googlemail.com>
2019-05-14 08:50:33 +0100

    Keep a list of trees so they can be rendered in the same place and with the same shape

--- src/Hill.js
+++ src/Hill.js
@@ -9,13 +9,15 @@ class Hill {
 	this.height = height;
 	this.y = y;
 	this.colour = colour;
+	this.line = new noisyLine(0, y, width, y, 50, 1);
+	this.trees = [];
+	this.genTrees = true;
     }
 
     draw() {
 	const ctx = Context.get();
 	const { width, height, y, colour } = this;
-	
-	const line = new noisyLine(0, y, width, y, 50, 1);
+	const { line } = this;
 
 	ctx.beginPath();
 	ctx.fillStyle = colour;
@@ -23,10 +25,10 @@ class Hill {
 	    const randomRange = (min, max) => {
 		return min + Math.random() * (max - min);
 	    };
-	    
-	    if (randomRange(0, 100) > 95) {
-		const tree = new Tree(x, y + 3, randomRange(2, 5), null, Math.floor(randomRange(4, 6)));
-		tree.draw();
+
+	    if (randomRange(0, 100) > 95 && this.genTrees) {
+		const tree = new Tree(x, y + 3, randomRange(2, 5), this.colour, Math.floor(randomRange(3, 5)));
+		this.trees.push(tree);
 	    }
 	});
 	ctx.lineTo(width, height);
@@ -34,6 +36,9 @@ class Hill {
 	ctx.lineTo(0, y);
 	ctx.fill();
 	ctx.closePath();
+	
+	this.trees.map(t => t.draw());
+	this.genTrees = false;
     }
 }
 
